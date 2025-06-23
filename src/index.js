416f2a180684743f1b39912f4094d225f22d759c
shrek2fan <151337012+shrek2fan@users.noreply.github.com>
2025-06-23 12:40:23 -0600

    Add Swagger UI documentation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const productRoutes = require('./routes/products');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -13,7 +16,10 @@ mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
   useUnifiedTopology: true
 });
 
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger', 'swagger.yaml'));
+
 app.use('/', productRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
